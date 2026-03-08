95612e42ccbc9318676d59b97d14cf0f6fe0ab17
Olha Prykhodko <o.u.prihodko@gmail.com>
2026-03-08 22:36:01 +0200

    feat: add SnippetSeedService to providers in SnippetModule

--- backend/src/modules/snippets/snippet.module.ts
+++ backend/src/modules/snippets/snippet.module.ts
@@ -3,13 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { snippetSchema } from '../../../models/Snippet.js';
 import { SnippetController } from './snippet.controller.js';
 import { SnippetService } from './snippet.service.js';
+import { SnippetSeedService } from './snippet.seed.js';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Snippet', schema: snippetSchema }]),
   ],
   controllers: [SnippetController],
-  providers: [SnippetService],
+  providers: [SnippetService, SnippetSeedService],
   exports: [],
 })
 export class SnippetModule {}
